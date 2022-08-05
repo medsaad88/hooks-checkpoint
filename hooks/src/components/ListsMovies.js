6ff3da49af78850c37be4a21e22b359ab879d92e
med saad <mohamedsaad.othman@gmail.com>
2022-08-05 23:47:01 +0100

    hooks

---
+++ hooks/src/components/ListsMovies.js
@@ -0,0 +1,58 @@
+
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"TOP GUN",
+    description:"Code-named Maverick, Pete Mitchell, an impetuous, daredevil Navy-pilot ace, is accepted into Top Gun, Miramar's elite Fighter School.",
+    posterUrl:"https://m.media-amazon.com/images/I/51QyUYRk-kL._AC_.jpg",
+    rate:5
+
+},
+{
+id:Math.random(),
+title:"Avengers",
+description:"The Asgardian Loki encounters the Other, the leader of an extraterrestrial race known as the Chitauri.",
+posterUrl:"https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._AC_SL1000_.jpg",
+rate:3
+
+},
+{
+    id:Math.random(),
+    title:"Fate Of the Furious 8 ",
+    description:"Dominic'dom'Toretto and Letty Ortiz are on their honeymoon in Havana when Dom's cousin Fernando gets in trouble owing money to local racer Raldo.",
+    posterUrl:"https://images-na.ssl-images-amazon.com/images/I/51wUNfZfAuL._AC_.jpg",
+    rate:5
+
+},
+{
+    id:Math.random(),
+    title:"الاختيار",
+    description:"تدور أحداث المسلسل حول حياة قائد الكتيبة 103 صاعقة في الجيش المصري عقيد أ.ح أحمد صابر منسي الذي قتل في كمين مربع البرث بمدينة رفح شمال سيناء، أثناء محاولته التصدي لهجوم مقاتلي دولة الخلافة الإسلامية في سيناء.",
+    posterUrl:"https://amayei.nyc3.digitaloceanspaces.com/2020/05/Choice.jpg",
+    rate:5
+
+},
+{
+    id:Math.random(),
+    title:"الاختيار2",
+    description:"يتناول المسلسل قصص حياة أفراد من الشرطة المصرية قتلوا في الفترة من 2013 وحتى 2020، كما يتناول العديد من الأحداث خلال تلك الفترة كفض رابعة العدوية وغيرها.",
+    posterUrl:"https://www.middleeasteye.net/sites/default/files/styles/article_page/public/images-story/2a1f336defadcc6d111563946d40dbc1.jpg?itok=Z5nLfitV",
+    rate:5
+
+},
+{
+    id:Math.random(),
+    title:"TOP GUN Maverick",
+    description:"Over thirty years after graduating from TOPGUN,United States Navy Captain Pete 'Maverick'Mitchell is a test pilot.",
+    posterUrl:"https://fr.web.img3.acsta.net/pictures/22/03/29/15/12/0827894.jpg",
+    rate:4
+
+},
+{
+    id:Math.random(),
+    title:"The Blacklist",
+    description:"Raymond 'Red'Reddington, an ex-U.S. Naval Intelligence officer who had disappeared twenty years earlier to become one of the FBI's Ten Most Wanted Fugitives.",
+    posterUrl:"https://fr.web.img5.acsta.net/pictures/210/051/21005165_20130808113654378.jpg",
+    rate:4
+
+},
+]
